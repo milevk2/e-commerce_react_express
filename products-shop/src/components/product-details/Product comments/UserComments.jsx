333b9bc6f72818960d32a3ea4d02264b4789c8ce
Mitko Milev <milevk2@gmail.com>
2023-12-06 17:11:23 +0200

    Added a component for comments section and the respective styles module

--- products-shop/src/components/product-details/Product comments/UserComments.jsx
+++ products-shop/src/components/product-details/Product comments/UserComments.jsx
@@ -1,43 +1,90 @@
+import { useEffect, useState } from 'react';
 import styles from './UserComments.module.css'
+import getDateTime from '../../../lib/getDateTime.js';
+import Comment from './Comment.jsx';
+import { updateProduct } from '../../../services/productService.js';
 
-const UserComments = () => {
 
+const UserComments = ({ comments, setComments, productId }) => {
 
+    const [rating, setRating] = useState(0);
+
+    async function addNewComment(e) {
+
+        e.preventDefault();
+        const formData = Object.fromEntries(new FormData(e.target));
+        formData.time = getDateTime();
+        formData.rating = rating;
+        formData.comment = true;
+        const updatedComments = [...comments, formData];
+
+        try {
+            const response = await updateProduct(productId, formData);
+            console.log(response);
+            setComments(updatedComments);
+        }
+        catch (err) {
+
+            console.log(err);
+        }
+        finally{e.target.reset()}
+    }
+
+    function ratingHandler(e) {
+
+        setRating(Number(e.target.id))
+        const stars = Array.from(e.currentTarget.children);
+
+        for (let star of stars) {
+
+            if (Number(star.id) <= Number(e.target.id)) {
+
+                star.classList.add(styles.checked);
+                continue;
+            }
+            star.classList.remove(styles.checked);
+        }
+    }
 
     return (
 
 
         <div id="user-comments" className={styles.commentWrapper}>
-            
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
             <h2 className="headerDiv" >Samsung Galaxy Tab A9+ - user opinions and reviews</h2>
 
-            <div className={styles.comment}>
+            {comments && comments.length > 0 && comments.map(data => (
+                <Comment comment={data} key={data._id || data.user_id} />
+            ))}
 
-                <div className={styles.userDetails}>
-                    <div className={styles.avatar}>D</div>
 
-                    <div className={styles.userName}>UserName</div>
+            <form className={styles.addComment} onSubmit={addNewComment}>
+
+                <label htmlFor='userName'>Your user name:</label>
+                <input className={styles.roundedBorder} name='userName'></input>
+
+                <label htmlFor='user_id'>Your user id:</label>
+                <input className={styles.roundedBorder} name='user_id'></input>
+
+                <label htmlFor='user_id'>Add comment:</label>
+                <textarea name='content' className={styles.roundedBorder}></textarea>
+
+                <label htmlFor='rating'>Your rating:</label>
+
+                <div className={styles.rating} onClick={ratingHandler}>
+
+                    <span className="fa fa-star" id='1'></span>
+                    <span className="fa fa-star" id='2'></span>
+                    <span className="fa fa-star" id='3'></span>
+                    <span className="fa fa-star" id='4'></span>
+                    <span className="fa fa-star" id='5'></span>
 
                 </div>
-                <div className={styles.comment}>Comment</div>
 
-                <div>This is a postDatetime</div>
+                <button type='submit' className='defaultButton'>Submit</button>
+            </form>
 
-            </div>
 
-            <div className={styles.comment}>
-
-                <div className={styles.userDetails}>
-                    <div className={styles.avatar}>D</div>
-
-                    <div className={styles.userName}>UserName</div>
-
-                </div>
-                <div className={styles.comment}>Comment2</div>
-
-                <div>This is a postDatetime</div>
-
-            </div>
 
         </div>
 
