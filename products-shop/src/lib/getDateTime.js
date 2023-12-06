8c1336d6851eb5a7980689dfda084e18e368ce48
Mitko Milev <milevk2@gmail.com>
2023-12-06 17:13:19 +0200

    getDateTime function added in the library

---
+++ products-shop/src/lib/getDateTime.js
@@ -0,0 +1,12 @@
+export default function getDateTime() {
+
+    const now = new Date();
+    const day = String(now.getDate()).padStart(2, '0');
+    const month = String(now.getMonth() + 1).padStart(2, '0');
+    const year = now.getFullYear();
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const formattedDateTime = `${day}/${month}/${year} ${hours}:${minutes}`;
+
+    return formattedDateTime;
+}
