e2a58ce99a335a0027bcc5b6a102f2f9757a1642
Mitko Milev <milevk2@gmail.com>
2023-12-08 23:19:09 +0200

    Login logic component added with the respective styling

---
+++ products-shop/src/components/user-login/LoginComponent.jsx
@@ -0,0 +1,56 @@
+import styles from './LoginComponent.module.css'
+import { login } from '../../services/userService.js'
+import { useNavigate } from 'react-router-dom'
+
+
+const LoginComponent = () => {
+
+    const navigate =useNavigate();
+
+    async function handleSubmit(e) {
+
+        e.preventDefault()
+        const formData = Object.fromEntries(new FormData(e.target))
+
+        try {
+
+          const response =  await login(formData);
+          const token = await response.json();
+           localStorage.setItem('authToken', token);
+            navigate('/')
+            
+        }
+        catch(err){
+
+            console.log('Login failed:', err);
+        }
+        
+    }
+
+    return (
+    
+    <form className={styles.loginPanel} onSubmit={handleSubmit}>
+        <label htmlFor="email">Email:</label>
+        <input
+          type="text"
+          id="email"
+          name="email"
+          className={styles.rounded}
+        />
+  
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          className={styles.rounded}
+        />
+  
+        <button type="submit" className={styles.submitButton}>
+          Login
+        </button>
+      </form>)
+
+}
+
+export default LoginComponent;
