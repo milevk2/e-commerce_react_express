35a90d9c7af1b100fe50431b298177cb4a4674d6
Mitko Milev <milevk2@gmail.com>
2023-12-08 23:21:38 +0200

    logout, login and register services added to the userService. Old code commented.

--- products-shop/src/services/userService.js
+++ products-shop/src/services/userService.js
@@ -1,113 +1,109 @@
-const baseUrl = 'http://localhost:3000/jsonstore/users';
+import * as request from '../lib/request.js'
 
-export const getAll = async () => {
-
-    try {
-        const response = await fetch(`${baseUrl}`)
-        const data = await response.json();
+const baseUrl = 'http://localhost:3000/users'
 
 
-        return Object.values(data);
-    }
-    catch (err) {
-
-        console.log(err);
-    }
-
-}
-
-export const getOne = async (id) => {
-
-    const request = await fetch(`${baseUrl}/${id}`)
-    const response = await request.json();
-
-    return response;
-}
 
 
 export const create = async (data) => {
 
     const body = {
 
-        firstName: data.firstName,
-        lastName: data.lastName,
-        email: data.email,
-        imageUrl: data.imageUrl,
-        phoneNumber: data.phoneNumber,
+        ...data,
         createdAt: new Date().toISOString(),
         updatedAt: new Date().toISOString(),
-        address: {
-
-            country: data.country,
-            city: data.city,
-            street: data.street,
-            streetNumber: data.streetNumber
-        }
-    }
-
-    return fetch(baseUrl, {
-
-        method: 'POST',
-
-        headers: {
-
-            'Content-Type': 'application/json',
-
-        },
-
-        body: JSON.stringify(body)
-
-    })//.then(response => response.json()).then(result => result).catch(err => console.log(err));
-}
-
-export const deleteUser = async (id) => {
-
-    const request = await fetch(`${baseUrl}/${id}`, {
-
-        method: 'DELETE',
-
-        headers: {
-
-            'Content-Type': 'application/json',
-        }
-
-    })
-    const response = await request.json();
-
-    return response;
-}
-
-export const editUser = async(id, data, createdAt) => {
-
-    const {firstName, lastName, email, phoneNumber, imageUrl} = data;
-
-    const address = {
-
-        country: data.country,
-        city: data.city,
-        street: data.street,
-        streetNumber: data.streetNumber
 
     }
 
-    const updatedBody = {firstName, lastName, email, phoneNumber, imageUrl, createdAt, updatedAt: new Date().toISOString(), address}
+    return await request.post(`${baseUrl}/register`, null, body);
+    
+}
 
-    console.log(`${JSON.stringify(updatedBody)} thiss is edited data!`);
+export const login = async (body) => {
 
-    const request = await fetch(`${baseUrl}/${id}`, {
-
-        method: 'PUT',
-
-        headers: {
-
-            'Content-Type': 'application/json',
-        },
-
-        body: JSON.stringify(updatedBody)
-
-    })
-    const response = await request.json();
-
-    return response;
+    return await request.post(`${baseUrl}/login`, null, body);
 
 }
+
+export const logout = async (token) => {
+
+    return await request.post(`${baseUrl}/logout`, null, {authToken: token});
+
+}
+
+
+// export const getAll = async () => {
+
+//     try {
+//         const response = await fetch(`${baseUrl}`)
+//         const data = await response.json();
+
+
+//         return Object.values(data);
+//     }
+//     catch (err) {
+
+//         console.log(err);
+//     }
+
+// }
+
+// export const getOne = async (id) => {
+
+//     const request = await request.get()
+//     const response = await request.json();
+
+//     return response;
+// }
+
+// export const deleteUser = async (id) => {
+
+//     const request = await fetch(`${baseUrl}/${id}`, {
+
+//         method: 'DELETE',
+
+//         headers: {
+
+//             'Content-Type': 'application/json',
+//         }
+
+//     })
+//     const response = await request.json();
+
+//     return response;
+// }
+
+// export const editUser = async(id, data, createdAt) => {
+
+//     const {firstName, lastName, email, phoneNumber, imageUrl} = data;
+
+//     const address = {
+
+//         country: data.country,
+//         city: data.city,
+//         street: data.street,
+//         streetNumber: data.streetNumber
+
+//     }
+
+//     const updatedBody = {firstName, lastName, email, phoneNumber, imageUrl, createdAt, updatedAt: new Date().toISOString(), address}
+
+//     console.log(`${JSON.stringify(updatedBody)} thiss is edited data!`);
+
+//     const request = await fetch(`${baseUrl}/${id}`, {
+
+//         method: 'PUT',
+
+//         headers: {
+
+//             'Content-Type': 'application/json',
+//         },
+
+//         body: JSON.stringify(updatedBody)
+
+//     })
+//     const response = await request.json();
+
+//     return response;
+
+// }
