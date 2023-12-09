330f7b38d8a937a264bb707eff97eb6b161bdb65
Mitko Milev <milevk2@gmail.com>
2023-12-09 23:28:04 +0200

    refactored the rating filler as it had stopped working. It seems that we were selecting dom elements prior to their react render.

--- products-shop/src/components/product-details/Product comments/Comment.jsx
+++ products-shop/src/components/product-details/Product comments/Comment.jsx
@@ -1,55 +1,44 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import styles from './UserComments.module.css'
 
+
 const Comment = ({ comment }) => {
-
+    const starsContainerRef = useRef(null);
+  
     useEffect(() => {
-
-        const element = document.getElementById(`${comment.user_id}`)
-
-        if (element) {
-            const stars = Array.from(element.children);
-
-            for (let star of stars) {
-
-                if (Number(star.id) <= Number(comment.rating)) {
-
-                    star.classList.add(styles.checked);
-                    continue;
-                }
-                star.classList.remove(styles.checked);
-            }
+      const stars = starsContainerRef.current?.children;
+      if (stars) {
+        for (let i = 0; i < stars.length; i++) {
+          const star = stars[i];
+          const isStarFilled = i + 1 <= Number(comment.rating);
+  
+          star.classList.toggle(styles.checked, isStarFilled);
         }
-    }, [])
-
-    if (comment.content !== undefined) return (
-
-
-        <div className={styles.comment}>
-
-            <div className={styles.userDetails}>
-                <div className={styles.avatar}>{comment.userName[0]}</div>
-                <div className={styles.userName}>{comment.userName}</div>
-            </div>
-
-            <div className={styles.commentText}>
-
-                <div>{comment.content}</div>
-
-                <div className={styles.rating} id={comment.user_id}>
-
-                    <span className="fa fa-star" id='1'></span>
-                    <span className="fa fa-star" id='2'></span>
-                    <span className="fa fa-star" id='3'></span>
-                    <span className="fa fa-star" id='4'></span>
-                    <span className="fa fa-star" id='5'></span>
-
-                </div>
-
-            </div>
-            <div className={styles.time}>{comment.time}</div>
-
-        </div>)
-}
+      }
+    }, [comment]);
+  
+    return (
+      <div className={styles.comment}>
+        <div className={styles.userDetails}>
+          <div className={styles.avatar}>{comment.userName[0]}</div>
+          <div className={styles.userName}>{comment.userName}</div>
+        </div>
+  
+        <div className={styles.commentText}>
+          <div>{comment.content}</div>
+  
+          <div className={styles.rating} ref={starsContainerRef}>
+            <span className="fa fa-star" id="1"></span>
+            <span className="fa fa-star" id="2"></span>
+            <span className="fa fa-star" id="3"></span>
+            <span className="fa fa-star" id="4"></span>
+            <span className="fa fa-star" id="5"></span>
+          </div>
+        </div>
+  
+        <div className={styles.time}>{comment.time}</div>
+      </div>
+    );
+  };
 
 export default Comment;
