7b76fc55d4ef40aea400271aae3333054e632e41
Mitko Milev <milevk2@gmail.com>
2023-12-09 13:54:44 +0200

    login component amended

--- products-shop/src/components/user-login/LoginComponent.jsx
+++ products-shop/src/components/user-login/LoginComponent.jsx
@@ -3,53 +3,54 @@ import { login } from '../../services/userService.js'
 import { useNavigate } from 'react-router-dom'
 
 
-const LoginComponent = () => {
+const LoginComponent = ({ setIsLogged }) => {
 
-    const navigate =useNavigate();
+  const navigate = useNavigate();
 
-    async function handleSubmit(e) {
+  async function handleSubmit(e) {
 
-        e.preventDefault()
-        const formData = Object.fromEntries(new FormData(e.target))
+    e.preventDefault()
+    const formData = Object.fromEntries(new FormData(e.target))
 
-        try {
+    try {
 
-          const response =  await login(formData);
-          const token = await response.json();
-           localStorage.setItem('authToken', token);
-            navigate('/')
-            
-        }
-        catch(err){
+      const response = await login(formData);
+      const token = await response.json();
+      localStorage.setItem('authToken', token);
+      setIsLogged(true)
+      navigate('/')
 
-            console.log('Login failed:', err);
-        }
-        
+    }
+    catch (err) {
+
+      console.log('Login failed:', err);
     }
 
-    return (
-    
+  }
+
+  return (
+
     <form className={styles.loginPanel} onSubmit={handleSubmit}>
-        <label htmlFor="email">Email:</label>
-        <input
-          type="text"
-          id="email"
-          name="email"
-          className={styles.rounded}
-        />
-  
-        <label htmlFor="password">Password:</label>
-        <input
-          type="password"
-          id="password"
-          name="password"
-          className={styles.rounded}
-        />
-  
-        <button type="submit" className={styles.submitButton}>
-          Login
-        </button>
-      </form>)
+      <label htmlFor="email">Email:</label>
+      <input
+        type="text"
+        id="email"
+        name="email"
+        className={styles.rounded}
+      />
+
+      <label htmlFor="password">Password:</label>
+      <input
+        type="password"
+        id="password"
+        name="password"
+        className={styles.rounded}
+      />
+
+      <button type="submit" className={styles.submitButton}>
+        Login
+      </button>
+    </form>)
 
 }
 
