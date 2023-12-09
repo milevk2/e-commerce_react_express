790c5d8d7106450ef3fbf10dbe2d7aaaeda2b03e
Mitko Milev <milevk2@gmail.com>
2023-12-09 23:22:35 +0200

    jwt now returns null if local storage is empty

--- products-shop/src/lib/jwtParser.js
+++ products-shop/src/lib/jwtParser.js
@@ -1,11 +1,9 @@
 export default function jwtParser() {
 
-    const jwtToken = localStorage.getItem('authToken')
+    if (!localStorage.getItem('authToken')) return null;
+    const jwtToken = localStorage.getItem('authToken');
     const [header, payload, signature] = jwtToken.split('.');
-    
     const decodedPayload = JSON.parse(atob(payload));
-    
-
     return decodedPayload;
 }
 
