5ad7fa3569daea5cfea1a5d6c4c1b974b03208f3
Mitko Milev <milevk2@gmail.com>
2023-12-09 23:21:57 +0200

    minor changes

--- products-shop/src/components/product-list/ProductList.jsx
+++ products-shop/src/components/product-list/ProductList.jsx
@@ -5,46 +5,36 @@ import styles from './ProductList.module.css'
 import jwtParser from "../../lib/jwtParser.js";
 
 
-const ProductList = ({ logged }) => {
+const ProductList = ({ myProducts }) => {
 
     const [products, setProducts] = useState([]);
 
-
     useEffect(() => {
 
-        if (logged) {
+        if (myProducts) {
 
             const decodedPayload = jwtParser();
             const userId = decodedPayload._id;
-
             getProduct(null, userId).then(products => setProducts([...products])).catch(err => console.log(err));
         }
         else {
-
             getProduct().then(products => setProducts([...products])).catch(err => console.log(err));
-
         }
-    }, [])
+    }, [myProducts])
 
     return (
-
         <>
             {
                 products.length == 0 ?
                     <div className={styles.notFound}>
-
                         <h1>Currently there are no products available...</h1>
-
                     </div>
                     :
                     <div>
-
                         <ul className={styles.shopWrapper}>
-
                             {products.map(product => <ProductListItem key={product._id} item={product} />)}
 
                         </ul>
-
                     </div>
             }
         </>
