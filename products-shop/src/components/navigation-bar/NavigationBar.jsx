fab1f6ebe7f14a72234b9113fec0453df9c0f88b
Mitko Milev <milevk2@gmail.com>
2023-12-09 23:28:55 +0200

    redirecting to home page after logout

--- products-shop/src/components/navigation-bar/NavigationBar.jsx
+++ products-shop/src/components/navigation-bar/NavigationBar.jsx
@@ -76,15 +76,13 @@ function NavigationBar({ cart, isLogged, setIsLogged, token, setToken, userId })
           <div className={styles.user}>
           <Nav.Link  as={NavLink} to={`/my_products`} style={linkStyle}>My Products</Nav.Link>
           <Nav.Link as={NavLink} to="/add_product" style={linkStyle}>Add Products</Nav.Link>
-          <Nav.Link as={NavLink} to="javascript:void(0)"style={linkStyle} onClick={onUserLogOut}>Logout</Nav.Link>
+          <Nav.Link as={NavLink} to="/"style={linkStyle} onClick={onUserLogOut}>Logout</Nav.Link>
           </div> 
           : 
           <div className={styles.guest}>
             <Nav.Link as={NavLink} to="/Register" style={linkStyle}>Register</Nav.Link>
             <Nav.Link as={NavLink} to="/Login" style={linkStyle}>Login</Nav.Link>
           </div>}
-          
-          
         </Nav>
 
         <div className={styles.cart}>
