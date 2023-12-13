41d61be61e5ac4a0a97cd116fe9d3288d6d597b7
Mitko Milev <milevk2@gmail.com>
2023-12-13 15:55:13 +0200

    Navigation bar responsiveness fixed - now the navigation links appear on top of each other when the screen is shrinked, so small-screen devises UI now appears way better

--- products-shop/src/components/navigation-bar/NavigationBar.jsx
+++ products-shop/src/components/navigation-bar/NavigationBar.jsx
@@ -4,7 +4,6 @@ import styles from './Navigation.module.css'
 import { Link, NavLink, useNavigate } from 'react-router-dom'
 import { logout } from '../../services/userService.js';
 
-
 function NavigationBar({ cart, isLogged, setIsLogged, token, setToken, userId }) {
 
   const [isHidden, setIsHidden] = useState(true);
@@ -67,37 +66,39 @@ function NavigationBar({ cart, isLogged, setIsLogged, token, setToken, userId })
           <path fillRule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"></path>
         </svg>
       </Navbar.Brand>
-      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" style={{ color: 'white' }} />
       <Navbar.Collapse id="responsive-navbar-nav">
         <Nav className="me-auto">
-          <Nav.Link as={NavLink} to="/" style={linkStyle}>Home</Nav.Link>
+          <div className={styles.encapsulated}>
+            <Nav.Link as={NavLink} to="/" style={linkStyle}>Home</Nav.Link>
 
-          <Nav.Link as={NavLink} to="/products" style={linkStyle}>Products</Nav.Link>
-          {isLogged ?
-            <div className={styles.user}>
-              <Nav.Link as={NavLink} to={`/my_products`} style={linkStyle}>My Products</Nav.Link>
-              <Nav.Link as={NavLink} to="/add_product" style={linkStyle}>Add Products</Nav.Link>
-              <Nav.Link as={NavLink} to="/profile" style={linkStyle}>Profile</Nav.Link>
-              <Nav.Link as={NavLink} to="/" style={linkStyle} onClick={onUserLogOut}>Logout</Nav.Link>
-              <div className={styles.cart}>
+            <Nav.Link as={NavLink} to="/products" style={linkStyle}>Products</Nav.Link>
+            {isLogged ?
+              <div className={styles.user}>
+                <Nav.Link as={NavLink} to={`/my_products`} style={linkStyle}>My Products</Nav.Link>
+                <Nav.Link as={NavLink} to="/add_product" style={linkStyle}>Add Products</Nav.Link>
+                <Nav.Link as={NavLink} to="/profile" style={linkStyle}>Profile</Nav.Link>
+                <Nav.Link as={NavLink} to="/" style={linkStyle} onClick={onUserLogOut}>Logout</Nav.Link>
+                <div className={styles.cart}>
 
-                <div className={` ${!isHidden ? styles.movingDiv : styles.hidden}`} role="alert">
-                  This item has been added to the cart!
-                </div>
+                  <div className={` ${!isHidden ? styles.movingDiv : styles.hidden}`} role="alert">
+                    This item has been added to the cart!
+                  </div>
 
-                <div className={styles.cartContainer}>
-                  <svg xmlns="http://www.w3.org/2000/svg" width="33" height="33" fill="currentColor" className="bi bi-cart cart" viewBox="0 0 16 16">
-                    <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"></path>
-                  </svg>
-                  <div className={`${styles.cartCounter} ${cartCounter < 1 ? styles.hidden : ''}`}>{`${cartCounter < 1 ? 0 : cartCounter}`}</div>
+                  <div className={styles.cartContainer}>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="33" height="33" fill="currentColor" className="bi bi-cart cart" viewBox="0 0 16 16">
+                      <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"></path>
+                    </svg>
+                    <div className={`${styles.cartCounter} ${cartCounter < 1 ? styles.hidden : ''}`}>{`${cartCounter < 1 ? 0 : cartCounter}`}</div>
+                  </div>
                 </div>
               </div>
-            </div>
-            :
-            <div className={styles.guest}>
-              <Nav.Link as={NavLink} to="/Register" style={linkStyle}>Register</Nav.Link>
-              <Nav.Link as={NavLink} to="/Login" style={linkStyle}>Login</Nav.Link>
-            </div>}
+              :
+              <div className={styles.guest}>
+                <Nav.Link as={NavLink} to="/Register" style={linkStyle}>Register</Nav.Link>
+                <Nav.Link as={NavLink} to="/Login" style={linkStyle}>Login</Nav.Link>
+              </div>}
+          </div>
         </Nav>
 
 
