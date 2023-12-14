9de726f507154020ac1fd30f08e265437823bd79
Mitko Milev <milevk2@gmail.com>
2023-12-14 23:52:37 +0200

    jwtParser no longer access the localStorage.

--- products-shop/src/lib/jwtParser.js
+++ products-shop/src/lib/jwtParser.js
@@ -1,7 +1,5 @@
-export default function jwtParser() {
+export default function jwtParser(jwtToken) {
 
-    if (!localStorage.getItem('authToken')) return null;
-    const jwtToken = localStorage.getItem('authToken');
     const [header, payload, signature] = jwtToken.split('.');
     const decodedPayload = JSON.parse(atob(payload));
     return decodedPayload;
