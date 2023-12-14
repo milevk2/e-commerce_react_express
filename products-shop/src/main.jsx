fe82c397b37aefb616f5bca3ced012f594ab24e1
Mitko Milev <milevk2@gmail.com>
2023-12-14 23:50:03 +0200

    wrapped the app between LoggerProvider, so we can have access to the LoggerContext all around the app

--- products-shop/src/main.jsx
+++ products-shop/src/main.jsx
@@ -3,15 +3,19 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom';
 import { LoadingProvider } from './LoadingContext.jsx';
+import { LoggerProvider } from './LoggerContext.jsx';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 
 
   <BrowserRouter>
-    <LoadingProvider>
-      <App />
-    </LoadingProvider>
+
+    <LoggerProvider>
+      <LoadingProvider>
+        <App />
+      </LoadingProvider>
+    </LoggerProvider>
   </BrowserRouter>,
 
 )
