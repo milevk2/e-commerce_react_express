e1945a3a39bf520699b6f6cb4ea1d54f51dad0a4
Mitko Milev <milevk2@gmail.com>
2023-12-15 21:37:11 +0200

    Added languageEnum with properties and attributes translated to en and bgn

---
+++ products-shop/src/language.js
@@ -0,0 +1,49 @@
+
+const language = {
+
+    en: {
+
+        name: 'Model',
+        announced: 'Announced',
+        display: 'Display Size',
+        display_format: 'Inches',
+        os: 'Operating System',
+        cpu: 'Central processing unit',
+        gpu: 'Graphic processing unit',
+        memory: 'Memory',
+        memory_format: 'RAM and Storage',
+        battery: 'Battery',
+        price: 'Price',
+        quantity: 'Quantity',
+        image: 'Image link',
+        category: 'Category',
+        edit: 'EDIT',
+        currency: 'bgn',
+        noProducts: 'Currently there are no products available...',
+        description: 'Description',
+      },
+
+      bg: {
+
+        name: 'Модел',
+        announced: 'Обявен',
+        display: 'Размер на екрана',
+        display_format: 'Инчове',
+        os: 'Операционна система',
+        cpu: 'Процесор',
+        gpu: 'Графичен Процесор',
+        memory: 'Памет',
+        memory_format: 'Рам и дисково пространство',
+        battery: 'Батерия',
+        price: 'Цена',
+        quantity: 'Количество',
+        image: 'Линк към снимка',
+        category: 'Категория',
+        edit: 'Промени',
+        currency: 'лв',
+        noProducts: 'Няма налични продукти...',
+        description: 'Описание',
+      }
+}
+
+export  {language};
