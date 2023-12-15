f6448d2b5dbe8ff19f77a178a2572a85d6c23c0b
Mitko Milev <milevk2@gmail.com>
2023-12-15 21:38:08 +0200

    added specsEnum and setSpecsEnum in order to switch between languages en-bg

--- products-shop/src/LanguageContext.jsx
+++ products-shop/src/LanguageContext.jsx
@@ -1,17 +1,24 @@
 import { createContext, useState } from 'react';
+import { language } from './language.js';
+
 
 const LanguageContext = createContext();
 
 const LanguageProvider = ({ children }) => {
 
   const [isEnglish, setIsEnglish] = useState(true);
+  const [specsEnum, setSpecsEnum] = useState({...language.en});
 
   const setLanguage = () => {
     setIsEnglish(state => state ? false : true)
+    setSpecsEnum(()=> {
+
+      return isEnglish ? {...language.bg} : {...language.en};
+    })
   };
 
   return (
-    <LanguageContext.Provider value={{ isEnglish, setLanguage }}>
+    <LanguageContext.Provider value={{ isEnglish, specsEnum, setLanguage }}>
       {children}
     </LanguageContext.Provider>
   );
