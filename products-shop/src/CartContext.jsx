5becc4d9b59349e2345c890108b41d4340b88024
Mitko Milev <milevk2@gmail.com>
2023-12-19 23:37:22 +0200

    Added CartContext for cart functionality in order to wrap it around the App

--- products-shop/src/CartContext.jsx
+++ products-shop/src/CartContext.jsx
@@ -6,25 +6,47 @@ const CartContext = createContext();
 const CartProvider = ({ children }) => {
 
   const [cartItems, setCartItems] = useState([]);
+  const [cartCounter, setCartCounter] = useState(0);
+  const [totalPrice, setTotalPrice] = useState(0)
+  const [isNotification, setIsNotification] = useState(false);
+
+  function calculateTotalPrice() {
+
+    cartItems.reduce((acc, curr) => {
+
+      acc += curr.price;
+
+      return acc;
+
+    }, 0)
+
+  }
+
 
   const addItem = (item) => {
-   setCartItems(items => [...items, item])
+    setIsNotification(true)
+    setTimeout(() => { setIsNotification(false) }, 50)
+    setCartItems(items => [...items, item]);
+    setCartCounter(x => x + 1);
+    setTotalPrice(total => total + item.price )
+   
   };
 
 
   const deleteItems = () => {
 
     setCartItems([]);
-
+    setCartCounter(0);
+    setTotalPrice(0);
   }
 
-//   const removeItem = (itemId) => {
+  //   const removeItem = (itemId) => {
 
 
-//   }
+  //   }
 
   return (
-    <CartContext.Provider value={{ cartItems, addItem, deleteItems }}>
+    <CartContext.Provider value={{ cartItems, cartCounter, totalPrice, addItem, isNotification, setIsNotification, deleteItems }}>
       {children}
     </CartContext.Provider>
   );
