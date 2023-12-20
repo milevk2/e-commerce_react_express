f8557e2289c64ed70fa4da6052db044beb58e975
Mitko Milev <milevk2@gmail.com>
2023-12-20 15:03:08 +0200

    Added a window size handler in order to keep the size state.CartComponent added which shows on small sceens. App.module.css deleted and the styles were moved to default.css

--- products-shop/src/App.jsx
+++ products-shop/src/App.jsx
@@ -13,37 +13,53 @@ import WeatherApi from './components/weather-api/WeatherApi.jsx';
 import Spinner from './components/Spinner.jsx';
 import { LoadingContext } from './LoadingContext.jsx';
 import { LoggerContext } from './LoggerContext.jsx';
-import styles from './App.module.css'
 import '../public/styles/default.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import CartComponent from './components/cart/CartComponent.jsx';
 
 
 function App() {
 
-  const [cart, setCart] = useState(false);
+
   const { isLoading } = useContext(LoadingContext);
   const { isLogged } = useContext(LoggerContext);
+  const [isSmallScreen, setIsSmallScreen] = useState(window.innerWidth < 575);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsSmallScreen(window.innerWidth < 575);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, [])
+
 
   return (
 
     <>
-      <NavigationBar cart={cart} />
+
+      <NavigationBar />
+      {isSmallScreen && isLogged && <div className='smallCart'><CartComponent /></div>}
       {isLoading && <Spinner />}
-      <div className={styles.main}>
+      <div className='main'>
         <WeatherApi />
         <Routes>
           <Route path="/" element={<HomeLoad />} />
-          <Route path="/products/:productId" element={<ProductDetails setCart={setCart} />} />
+          <Route path="/products/:productId" element={<ProductDetails isSmallScreen={isSmallScreen} />} />
           <Route path="/add_product" element={isLogged ? <AddProductForm /> : <NotFound />} />
           <Route path="/products" element={<ProductList />} />
           <Route path="/my_products" element={<ProductList myProducts={true} />} />
           <Route path="/Register" element={!isLogged ? <RegisterComponent /> : <NotFound />} />
-          <Route path="/Login" element={!isLogged ? <LoginComponent /> : <NotFound />} /> 
+          <Route path="/Login" element={!isLogged ? <LoginComponent /> : <NotFound />} />
           <Route path="/profile" element={isLogged && <UserProfile />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </div>
-      <footer className={styles.footer}><p>All rights reserved &copy;</p></footer>
+      <footer className='footer'><p>All rights reserved &copy;</p></footer>
     </>
   )
 }
