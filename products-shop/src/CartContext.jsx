f2f2ee5aea303f5b62c908b24a660d2aa2059570
Mitko Milev <milevk2@gmail.com>
2023-12-20 15:05:24 +0200

    cart context updated, logic slightly changed. also resetContextState functionality added, it should be executed on user logout

--- products-shop/src/CartContext.jsx
+++ products-shop/src/CartContext.jsx
@@ -2,7 +2,6 @@ import { createContext, useState } from 'react';
 
 const CartContext = createContext();
 
-
 const CartProvider = ({ children }) => {
 
   const [cartItems, setCartItems] = useState([]);
@@ -10,28 +9,23 @@ const CartProvider = ({ children }) => {
   const [totalPrice, setTotalPrice] = useState(0)
   const [isNotification, setIsNotification] = useState(false);
 
-  function calculateTotalPrice() {
+  function resetContextState() {
 
-    cartItems.reduce((acc, curr) => {
-
-      acc += curr.price;
-
-      return acc;
-
-    }, 0)
+    setCartItems([]);
+    setCartCounter(0);
+    setTotalPrice(0);
+    setIsNotification(false);
 
   }
 
-
   const addItem = (item) => {
-    setIsNotification(true)
-    setTimeout(() => { setIsNotification(false) }, 50)
+    setIsNotification(true);
+    setTimeout(() => { setIsNotification(false) },50);
     setCartItems(items => [...items, item]);
     setCartCounter(x => x + 1);
-    setTotalPrice(total => total + item.price )
-   
-  };
+    setTotalPrice(total => total + item.price);
 
+  };
 
   const deleteItems = () => {
 
@@ -41,12 +35,12 @@ const CartProvider = ({ children }) => {
   }
 
   //   const removeItem = (itemId) => {
-
+      //to be done
 
   //   }
 
   return (
-    <CartContext.Provider value={{ cartItems, cartCounter, totalPrice, addItem, isNotification, setIsNotification, deleteItems }}>
+    <CartContext.Provider value={{ cartItems, cartCounter, totalPrice, addItem, isNotification, setIsNotification, deleteItems, resetContextState }}>
       {children}
     </CartContext.Provider>
   );
