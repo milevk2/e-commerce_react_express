735a11bc2c6d6843a2137d90e686e768f3a05ffd
Mitko Milev <milevk2@gmail.com>
2023-12-20 16:44:17 +0200

    electronicsSchema added in the users model inside cart: [electronicsSchema]

--- server/src/models/Electronics.js
+++ server/src/models/Electronics.js
@@ -1,14 +1,4 @@
 const mongoose = require('mongoose');
-// ⦁	name – string (required)
-// ⦁	type – string (required)
-// ⦁	damages – string (required)
-// ⦁	image - string (required)
-// ⦁	description – string (required)
-// ⦁	production – number (required)
-// ⦁	exploitation - number (required)
-// ⦁	price - number (required)
-// ⦁	buyingList – an array of objects containing the users' ID
-// ⦁	owner – object ID (a reference to the User model)
 
 const userCommentsSchema = new mongoose.Schema({
     content: String,
@@ -16,60 +6,60 @@ const userCommentsSchema = new mongoose.Schema({
     user_id: String,
     time: String,
     rating: Number
-  });
+});
 
 const electronicsSchema = new mongoose.Schema({
-    name: { 
-        type: String, 
+    name: {
+        type: String,
         required: true
     },
-    announced:{ 
-        type: String, 
+    announced: {
+        type: String,
         required: true
     },
-    displaySize: { 
-        type: String, 
+    displaySize: {
+        type: String,
         required: true
     },
-    image: { 
-        type: String, 
+    image: {
+        type: String,
         required: true
     },
-    description: { 
-        type: String, 
+    description: {
+        type: String,
         required: true
     },
-    operating_system: { 
-        type: String,  
+    operating_system: {
+        type: String,
         required: true
     },
-    cpu: { 
-        type: String, 
+    cpu: {
+        type: String,
         required: true
     },
-    gpu: { 
-        type: String,  
+    gpu: {
+        type: String,
         required: true
     },
-    ram: { 
-        type: String, 
+    ram: {
+        type: String,
         required: true
     },
-    price: { 
-        type: Number, 
+    price: {
+        type: Number,
         required: true
     },
-    battery: { 
-        type: String, 
+    battery: {
+        type: String,
         required: true
     },
-    quantity: { 
-        type: String, 
+    quantity: {
+        type: String,
         required: true
     },
 
-    storage: { 
-        type: String, 
+    storage: {
+        type: String,
         required: true
     },
 
@@ -81,7 +71,7 @@ const electronicsSchema = new mongoose.Schema({
     },
 
     comments: [userCommentsSchema],
-    
+
     ownerId: {
         type: String,
         required: true
@@ -90,10 +80,8 @@ const electronicsSchema = new mongoose.Schema({
     owner: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User',
-      },
-
+    },
 });
 
-
 const Electronics = mongoose.model("Electronics", electronicsSchema);
-module.exports = Electronics;
+module.exports = { electronicsSchema, Electronics };
--- server/src/models/User.js
+++ server/src/models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+const {electronicsSchema} = require('./Electronics.js');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
@@ -8,6 +9,7 @@ const userSchema = new Schema({
   email: String,
   password: String,
   createdAt: Date,
+  cart: [electronicsSchema],
 });
 
 
