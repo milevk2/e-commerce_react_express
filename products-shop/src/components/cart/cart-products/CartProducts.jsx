3e5a63fc2cb184d5a31336e7b30849e3951da0ed
Mitko Milev <milevk2@gmail.com>
2024-01-10 21:37:37 +0200

    Cart products container was improved so it now contains buyQuantity and total price for every item. Styling adjusted accordingly. Product items were wrapped with NavLink which forwards to the item details component when clicked

--- products-shop/src/components/cart/cart-products/CartProducts.jsx
+++ products-shop/src/components/cart/cart-products/CartProducts.jsx
@@ -1,28 +1,25 @@
-import { useContext, useEffect, useState } from 'react';
+import { useContext } from 'react';
 import styles from './CartProducts.module.css'
 import { CartContext } from '../../../CartContext.jsx';
 import CartItem from './cart-item/CartItem.jsx';
-
+import { NavLink } from 'react-router-dom';
 
 
 const CartProducts = () => {
 
-    const { cartItems, cartCounter, totalPrice, addItem,  deleteItems } = useContext(CartContext);
+    const { cartItems, cartCounter, totalPrice, deleteItems } = useContext(CartContext);
 
     return (
-
         <>
             {cartCounter > 0 && <div className={styles.cartProducts}>
 
                 {cartItems.map(item => <CartItem key={item._id} item={item} />)}
 
-                <div className={styles.total}>Total: {totalPrice.toFixed(2)} bgn</div>
+                <div className={styles.total}>Total: {totalPrice.toFixed(2)} BGN</div>
                 <button className={styles.clearCart} onClick={() => {
-        
                     deleteItems();
                 }
                 }>Delete items</button>
-
             </div>}
         </>
     )
--- products-shop/src/components/cart/cart-products/cart-item/CartItem.jsx
+++ products-shop/src/components/cart/cart-products/cart-item/CartItem.jsx
@@ -1,6 +1,7 @@
 import { useContext } from 'react'
 import styles from './CartItem.module.css'
 import { LanguageContext } from '../../../../LanguageContext.jsx'
+import { NavLink } from 'react-router-dom';
 
 const CartItem = ({item}) => {
 
@@ -8,16 +9,19 @@ const CartItem = ({item}) => {
 
     return (
 
+        <NavLink to={`/products/${item._id}`} className={styles.link}>
         <div className={styles.cartRow}>
 
             <div ><img src={item.image} className={styles.itemImage}/></div>
 
             <div className={styles.itemName}>{item.name}</div>
 
-            <div className={styles.itemPrice}>{item.price} {specsEnum.currency}</div>
+           <div className={styles.itemName}>Quantity: {`${item.buyQuantity}x${item.price}`}</div>
+
+            <div className={styles.itemPrice}>{(item.price * item.buyQuantity).toFixed(2)} {specsEnum.currency}</div>
 
         </div>
-
+        </NavLink>
 
     )
 
