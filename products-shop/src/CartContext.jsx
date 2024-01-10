89a4e82fa3ec8830893c1cd9f9535564b35d7154
Mitko Milev <milevk2@gmail.com>
2024-01-10 21:55:10 +0200

    Cart management state thoroughly rewritten. Now the cart items are not duplicating if user adds certain product more than once a buyQuantity property is being incremented. The total cart amount is being calculated according to item price times item quantity.addItem function improved and adjusted according to the changes

--- products-shop/src/CartContext.jsx
+++ products-shop/src/CartContext.jsx
@@ -1,4 +1,7 @@
-import { createContext, useState } from 'react';
+import { createContext, useContext, useEffect, useState } from 'react';
+import jwtParser from './lib/jwtParser.js';
+import { update } from './services/userService.js';
+import { LoggerContext } from './LoggerContext.jsx';
 
 const CartContext = createContext();
 
@@ -8,6 +11,46 @@ const CartProvider = ({ children }) => {
   const [cartCounter, setCartCounter] = useState(0);
   const [totalPrice, setTotalPrice] = useState(0)
   const [isNotification, setIsNotification] = useState(false);
+  const [isToken, setIsToken] = useState('');
+  const { userId } = useContext(LoggerContext);
+
+  useEffect(() => {
+
+    if (cartItems.length > 0) {
+
+      let temp_totalPrice = 0;
+      let temp_cartCounter = 0;
+
+      for (let item of cartItems) {
+
+        temp_totalPrice += (item.price * item.buyQuantity);
+        temp_cartCounter += item.buyQuantity;
+      }
+
+      setCartCounter(temp_cartCounter);
+      setTotalPrice(temp_totalPrice);
+
+      // setCartCounter(cartItems.reduce((acc, curr) => {
+
+      //   return acc + (curr.buyQuantity ? curr.buyQuantity : 1)
+
+      // }, 0));
+
+    }
+
+    if (userId) updateCartDb();
+
+  }, [cartItems])
+
+
+  async function updateCartDb() {
+
+    await update({
+
+      _id: userId,
+      cart: cartItems
+    })
+  }
 
   function resetContextState() {
 
@@ -15,16 +58,25 @@ const CartProvider = ({ children }) => {
     setCartCounter(0);
     setTotalPrice(0);
     setIsNotification(false);
-
   }
 
   const addItem = (item) => {
+
     setIsNotification(true);
-    setTimeout(() => { setIsNotification(false) },50);
-    setCartItems(items => [...items, item]);
+    setTimeout(() => { setIsNotification(false) }, 50);
     setCartCounter(x => x + 1);
     setTotalPrice(total => total + item.price);
 
+    for (let currItem of cartItems) {
+
+      if (item._id === currItem._id ) {
+
+        currItem.buyQuantity += 1;
+        setCartItems(items => [...items]);
+        return;
+      }
+    }
+    setCartItems(items => [...items, item]);
   };
 
   const deleteItems = () => {
@@ -35,12 +87,12 @@ const CartProvider = ({ children }) => {
   }
 
   //   const removeItem = (itemId) => {
-      //to be done
+  //to be done
 
   //   }
 
   return (
-    <CartContext.Provider value={{ cartItems, cartCounter, totalPrice, addItem, isNotification, setIsNotification, deleteItems, resetContextState }}>
+    <CartContext.Provider value={{ cartItems, setCartItems, cartCounter, setCartCounter, totalPrice, setTotalPrice, addItem, isNotification, setIsNotification, deleteItems, resetContextState, setIsToken }}>
       {children}
     </CartContext.Provider>
   );
