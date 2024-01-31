f973b722ca181ae99c980853d4b3f60d56cbeb72
Mitko Milev <milevk2@gmail.com>
2024-01-31 23:58:41 +0200

    homepage component added with integrated news API. Needs style fixes

---
+++ products-shop/src/components/home-page/HomeComponent.jsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react"
+import callUtilityService from "../../services/utility.js";
+import styles from "./HomeComponent.module.css"
+
+
+const HomeComponent = () => {
+
+    const [news, setNews] = useState([]);
+
+    useEffect(() => {
+
+        callUtilityService('news').then(data => data.json()).then(result => {
+
+
+            let arr = result["articles"]
+            setNews(arr.slice(1, 10));
+            console.log(arr);
+        }
+        ).catch(err => console.log('The news will not be updated!'))
+
+
+    }, [])
+
+
+    return (
+
+
+        <div className={styles.news}>
+
+            <h2 className="headerDiv">LATEST TECH NEWS</h2>
+            {news.map(article =>
+            
+            <article className={styles.card}>
+                <img  src={article.urlToImage} alt={article.title} />
+                <h5>{article.title}</h5>
+                <p>{article.description}</p>
+                <a href={article.url}>Read more</a>
+                </article>)}
+        </div>
+    )
+}
+
+export default HomeComponent;
