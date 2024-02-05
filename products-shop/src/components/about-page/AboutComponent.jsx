0bda64a62e25380f7f9bafc6a2dae70af99e1c86
Mitko Milev <milevk2@gmail.com>
2024-02-05 19:18:57 +0200

    About Page totally redone with actual information about the application. Styling significantly improved

--- products-shop/src/components/about-page/AboutComponent.jsx
+++ products-shop/src/components/about-page/AboutComponent.jsx
@@ -1,9 +1,6 @@
 import { useContext } from "react";
 import { LanguageContext } from "../../LanguageContext.jsx";
-import  './about.css'
-
-
-
+import './about.css'
 
 const AboutComponent = () => {
 
@@ -11,83 +8,67 @@ const AboutComponent = () => {
 
     return (
         <>
-            {isEnglish ? <div className="welcome-text">
-                <h1>Welcome to Bultelecom Shop - Your Ultimate Destination for Smartphones!</h1>
-
-                <p>Discover the latest in cutting-edge technology and elevate your mobile experience with our premium selection
-                    of smartphones. At Bultelecom Shop, we bring you a curated collection of the most sought-after devices from top
-                    brands.</p>
-
-                <h2>Explore Innovation</h2>
-                <p>Immerse yourself in the world of innovation and stay ahead with our range of smartphones featuring the latest
-                    technologies, sleek designs, and powerful performance.</p>
-
-                <h2>Shop with Confidence</h2>
-                <p>Enjoy a seamless shopping experience with secure transactions, fast shipping, and hassle-free returns. Our
-                    customer support team is here to assist you every step of the way.</p>
-
-                <h2>Exclusive Deals</h2>
-                <p>Unlock exclusive deals and promotions, ensuring you get the best value for your money. Keep an eye out for
-                    special offers and limited-time discounts on your favorite devices.</p>
-
-                <h2>Global Brands, Local Service</h2>
-                <p>We partner with globally renowned brands to bring you smartphones that redefine excellence. Rest assured, our
-                    local service ensures you get the support you need, whenever you need it.</p>
-
-                <h2>Why Choose Us?</h2>
-                <ul className="feature-list">
-                    <li>Wide Range of Smartphones</li>
-                    <li>Secure and Convenient Shopping</li>
-                    <li>Expert Customer Support</li>
-                    <li>Exclusive Offers and Discounts</li>
-                </ul>
-
-                <p>Welcome to the future of mobile technology! Start exploring our collection now and find the perfect smartphone
-                    that suits your lifestyle.</p>
-
-                <p>Happy shopping at Bultelecom Shop!</p>
-            </div>
-                :
+            <div>
                 <div className="welcome-text">
-                    <h1>Добре дошли в Bultelecom Shop - Вашият най-добър избор за смартфони!</h1>
 
-                    <p>Открийте най-новите постижения в технологиите и подобрете своето мобилно преживяване с нашата премиум селекция от
-                        смартфони. В Bultelecom Shop ви представяме подбрана колекция от най-търсените устройства от водещи марки.</p>
+                    <h1>E-Commerce React Express Project</h1>
+                    {isEnglish ?
+                        <>
+                            <p>
+                                This is my MERN project which I use to train my knowledge on. It is not perfect - there are still many bugs
+                                and not implemented functionalities and there are many things I have to work on. The main purpose is to train
+                                my frontend, backend, database, api and design skills.
+                            </p>
 
-                    <h2>Разгледайте Иновациите</h2>
-                    <p>Поглезете се в света на иновациите и бъдете на преден план с нашата гама от смартфони, предлагащи най-новите
-                        технологии, елегантен дизайн и мощна производителност.</p>
 
-                    <h2>Пазарувайте с Увереност</h2>
-                    <p>Научете се на безпроблемно пазаруване със защитени транзакции, бърза доставка и безпроблемни връщания. Нашите
-                        служители от екипа за клиентска поддръжка са тук, за да ви помогнат на всяка стъпка.</p>
+                            <ul className="feature-list">
+                                <li>React.js for dynamic and responsive user interfaces.</li>
+                                <li>Express.js server for handling backend logic and APIs.</li>
+                                <li>Integration with a backend database for product management.</li>
+                                <li>Mongoose as the MongoDB ODM for database integration.</li>
+                                <li>Web-scraping service for generating dummy shop data (in development only).</li>
+                                <li>User authentication and authorization for a personalized shopping experience.</li>
+                                <li>Bulgarian version of the site available through LanguageContext.jsx. It takes the language from language enum in language.js.</li>
+                                <li>Weather and News APIs integrated for user convenience.</li>
+                                <li>Cache logic implemented on the backend for handling the limited API requests and data.</li>
+                            </ul>
+                        </>
+                        :
+                        <>
+                            <p>
+                                Това е моят MERN проект, който използвам за трениране на знанията си. Той не е перфектен - все още има много бъгове и нереализирани функционалности,
+                                и има още много неща, по които трябва да работя. Основната цел е да тренирам уменията си във frontend, backend, бази данни, API и дизайн.
+                            </p>
 
-                    <h2>Ексклузивни Оферти</h2>
-                    <p>Отключете ексклузивни сделки и промоции, гарантирайки си най-добрата стойност за вашия пари. Внимавайте за
-                        специални предложения и ограничени времеви отстъпки за вашите любими устройства.</p>
+                            <ul className="feature-list">
+                                <li>React.js за динамични и отзивчиви потребителски интерфейси.</li>
+                                <li>Express.js сървър за обработка на бекенд логика и API.</li>
+                                <li>Интеграция със заден край за управление на продукти в базата данни.</li>
+                                <li>Mongoose като MongoDB ODM за интеграция с базата данни.</li>
+                                <li>Уеб скрейпинг услуга за генериране на фиктивни данни за магазин (само при разработка).</li>
+                                <li>Потребителска аутентикация и авторизация за персонализирано пазаруване.</li>
+                                <li>Българска версия на сайта, достъпна чрез LanguageContext.jsx. Взема езика от енумерацията на езика в language.js.</li>
+                                <li>Интеграция на API за времето и новини за удобство на потребителите.</li>
+                                <li>Логика за кеширане, реализирана на бекенда за обработка на ограничените заявки и данни.</li>
+                            </ul>
+                        </>
+                    }
 
-                    <h2>Глобални Марки, Локално Обслужване</h2>
-                    <p>Сътрудничим с глобално признати марки, за да ви представим смартфони, които преопределят отличието. Можете да
-                        бъдете уверени, че нашето локално обслужване гарантира подкрепата, която ви е необходима, когато ви е нужна.</p>
+                    <div className="apiContainer">
 
-                    <h2>Защо Да Изберете Нас?</h2>
-                    <ul className="feature-list">
-                        <li>Широка Гама от Смартфони</li>
-                        <li>Защитено и Удобно Пазаруване</li>
-                        <li>Експертна Клиентска Поддръжка</li>
-                        <li>Ексклузивни Предложения и Отстъпки</li>
-                    </ul>
-
-                    <p>Добре дошли в бъдещето на мобилните технологии! Започнете да разглеждате нашата колекция сега и намерете
-                        перфектния смартфон, който отговаря на вашия начин на живот.</p>
-
-                    <p>Щастливо пазаруване в Bultelecom Shop!</p>
+                        <a href="https://weatherstack.com/" target="_blank" className="item"><div className="weatherApi"><img src="https://weatherstack.com/site_images/weatherstack_logo_white.png" alt="WeatherStack API" /></div></a>
+                        <a href="https://newsapi.org/" target="_blank" className="item">
+                            <div className="newsApi">
+                                <span className="blue">News </span>
+                                <span className="white">API</span>
+                            </div>
+                        </a>
+                        <a href="https://github.com/milevk2/e-commerce_react_express.git" target="_blank" className="item"><div className="gitHub">Project's GitHub:<i className="fa-brands fa-github icon"></i></div></a>
+                    </div>
                 </div>
-
-            }
+            </div>
         </>
     )
-
 }
 
 export default AboutComponent;
