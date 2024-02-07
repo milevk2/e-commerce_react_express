ff7ec9cd7159964d839a6de5ac57ecacfd77a7b1
Mitko Milev <milevk2@gmail.com>
2024-02-07 14:25:16 +0200

    Added a conditional rendering for the  weatherAPI

--- products-shop/src/components/weather-api/WeatherApi.jsx
+++ products-shop/src/components/weather-api/WeatherApi.jsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from "react"
-import { get } from '../../lib/request.js'
 import styles from './WeatherApi.module.css'
-import {VITE_API_URL} from '../../services/host.js'
+import callUtilityService from "../../services/utility.js";
+// import {VITE_API_URL} from '../../services/host.js'
+// import { get } from '../../lib/request.js'
+// const url = `${VITE_API_URL}/getWeather`
 
-const url = `${VITE_API_URL}/getWeather`
+
 
 const WeatherApi = () => {
 
@@ -12,7 +14,7 @@ const WeatherApi = () => {
 
     async function getWeatherData() {
 
-        const response = await get(url, { 'Cache-Control': 'no-store'})
+        const response = await callUtilityService('getWeather'); /*await get(url, { 'Cache-Control': 'no-store'}) */
 
         if (!response.ok) setError(true);
 
@@ -26,33 +28,37 @@ const WeatherApi = () => {
             icon: newData.current.weather_icons[0],
             description: newData.current.weather_descriptions[0]
         }
-        setWeatherData({...filteredData});
+        setWeatherData({ ...filteredData });
     }
 
     useEffect(() => {
 
         getWeatherData();
-        const updateInterval = 1200 * 1000  //20 mins
+        const updateInterval = 20 * 60 * 1000; //20 mins
         const intervalId = setInterval(getWeatherData, updateInterval);
-        
+
         return () => {
             clearInterval(intervalId);
-          };
-    
-    },[])
+        };
+
+    }, [])
 
     return (
+        <>
+            {error == true || !weatherData.city ? ''
+                :
+                <div className={styles.weatherBox}>
+                    <a href='https://weatherstack.com/' className={styles.weatherBoxA}>
 
-        <div className={styles.weatherBox}>
-        <a href='https://weatherstack.com/' className={styles.weatherBoxA}>
-       
-            <div className={styles.weatherSpec}>{weatherData.city}</div>
-            <div className={styles.weatherSpec}><img src={weatherData.icon}></img></div>
-            <div className={styles.weatherSpec}>{weatherData.temperature}°C</div>
-            <div className={styles.weatherSpec}>{weatherData.description}</div>
-            <div className={styles.weatherSpec}>{weatherData.time}</div>
-        </a>
-        </div>
+                        <div className={styles.weatherSpec}>{weatherData.city}</div>
+                        <div className={styles.weatherSpec}><img src={weatherData.icon}></img></div>
+                        <div className={styles.weatherSpec}>{weatherData.temperature}°C</div>
+                        <div className={styles.weatherSpec}>{weatherData.description}</div>
+                        <div className={styles.weatherSpec}>{weatherData.time}</div>
+                    </a>
+                </div>
+            }
+        </>
     )
 }
 
