c0d2b6c59871e9428cd819ab058e85f3b2dd3406
Mitko Milev <milevk2@gmail.com>
2024-02-07 14:24:28 +0200

    fetch api link changed to `${VITE_API_URL}/api/${endpoint}` so the api services could be moved to a separate route on the backend

--- products-shop/src/services/utility.js
+++ products-shop/src/services/utility.js
@@ -2,8 +2,7 @@ import { VITE_API_URL } from './host.js'
 
 function callUtilityService(endpoint) {
 
-    return fetch(`${VITE_API_URL}/${endpoint}`);
-
+    return fetch(`${VITE_API_URL}/api/${endpoint}`);
 }
 
 export default callUtilityService;
